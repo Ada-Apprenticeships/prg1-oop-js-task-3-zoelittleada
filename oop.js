390d47f6de2442925063217baa8a63f7abfbd4b1
zoelittleada <zoe.little@ada.ac.uk>
2024-10-25 13:27:45 +0000

    validInteger function

--- oop.js
+++ oop.js
@@ -2,9 +2,11 @@ PRIORITY = { "LOW": 1, "MEDIUM": 3, "HIGH": 5, "URGENT": 7 };
 
 
 function validInteger (value) { // value can be a string or a number (integer)
-  
+  const num = Number(value);
+  return !isNaN(num) && Number.isInteger(num) && num>0;
 }  
 
+console.log(validInteger(18))
 
 function validatePriority(priority) { // value can be a string or a number (integer)
   
