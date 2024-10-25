be000035e388bd532f2d844907129c80c9e8c082
zoelittleada <zoe.little@ada.ac.uk>
2024-10-25 14:28:55 +0000

    class task and to do in progress

--- oop.js
+++ oop.js
@@ -12,27 +12,54 @@ function validatePriority(priority) { // value can be a string or a number (inte
 
 
 function todaysDate() {
-  const today = new Date()
-  const year = today.getFullYear()
-  const month = today.getMonth().toString().padStart(2,'0')
-  const day = today.getDay().toString().padStart(2,'0')
-  const hours = today.getHours().toString().padStart(2,'0')
-  const minutes = today.getMinutes().toString().padStart(2,'0')
-  const seconds = today.getSeconds().toString().padStart(2,'0')
-  return`${day}/${month}/${year} ${hours}:${minutes}:${seconds}`
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth().toString().padStart(2,'0');
+  const day = today.getDay().toString().padStart(2,'0');
+  const hours = today.getHours().toString().padStart(2,'0');
+  const minutes = today.getMinutes().toString().padStart(2,'0');
+  const seconds = today.getSeconds().toString().padStart(2,'0');
+  return`${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
 }
-
-console.log(todaysDate())
 
 
 class Task  {
+  constructor(added=todaysDate(), title, priority){
+    this._added = added;
+    this._title = title;
+    this._priority = priority;
+  }
 
-  // (title, priority)
+  get added(){
+    return this._added;
+  }
+
+  get title(){
+    return this._title;
+  }
+
+  get priority(){
+    return this._priority;
+  }
+  set priority(newPriority){
+    this._priority = validatePriority(newPriority);
+  }
 }
 
 
 class ToDo {
+  constructor(){
+    this._task = [];
+  }
+  
+  add(task){
+    this._tasks.push(task);
+    return this._tasks.length;
+  }
+
+  remove(title){
     
+  }
 }
 
 
